6c8c6f57bbc0ee11e52135e8fe032257189d5b69
adamsmith43556-commits <adamsmith43556@gmail.com>
2026-05-03 13:55:02 +0100

    Add Home component to index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif', color: '#333' }}>
+      <h1>Node Status: Active</h1>
+      <p>Data synchronization and optimization in progress.</p>
+      <small>v2.4.1-stable</small>
+    </div>
+  );
+}
+
